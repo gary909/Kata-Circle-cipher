a1ca8c5f1e1d7bab0f76ddfd12c60139a86e997f
gary909 <garywhite61@hotmail.com>
2022-10-21 13:59:31 +0200

    gave up :-(

--- app.js
+++ app.js
@@ -1,4 +1,38 @@
+// I gave up!
+
+
+
 function encode(s) {
+    let myList = s.split('');
+    let myRevList = s.split('').reverse();
+    let myRes = '';
+    let myRes2 = '';
+    let myFinal = ''
+    let myNum = myList.length - 1;
+    for (var i = 0; i < myList.length / 2; i = i + 1) {
+        myFinal = myFinal + myList[i];
+        myFinal = myFinal + myList[myNum];
+        myNum - 1
+    }
+
+
+
+    // for (var i = 0; i < myRes.length / 2; i++) {
+    //     //let myNum = myRes2.length - 1;
+    //     myFinal = myFinal + myRes[i];
+    //     myFinal = myFinal + myRes2[i];
+    //     myFinal = myFinal + myRes2[myNum];
+    //     myFinal = myFinal + myRes[myNum];
+    //     myNum = myNum - 1;
+    // }
+
+
+    // for (var i = 0; i < myList.length; i = i + 2) {
+    // myFinal = myRes[0] + myRes2[0] + myRes2[3] + myRes[3] + myRes[1] + myRes2[1] + myRes2[2] + myRes[2]
+    // }
+
+    // return myNum - 1;
+    return myFinal;
 }
 
 function decode(s) {
